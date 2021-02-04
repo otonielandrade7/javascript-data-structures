f595f752c2d62fcdb0b8144c943badcdac175744
Otoniel Andrade <otonielandrade7@gmail.com>
2021-02-04 10:12:21 -0600

    queues data structure finished

--- queues/queues.js
+++ queues/queues.js
@@ -6,26 +6,25 @@ class Node {
 }
 class Queue {
   constructor() {
-    this.top = null;
-    this.bottom = null;
+    this.first = null;
+    this.last = null;
     this.length = 0;
   }
-  unqueue(value) {
+  enqueue(value) {
     const newNode = new Node(value);
     if (this.length === 0) {
-      this.bottom = newNode;
-      this.top = this.bottom;
+      this.first = newNode;
+      this.last = this.first;
     } else {
-      const holdingNode = this.bottom;
-      this.bottom = newNode;
-      this.bottom.next = holdingNode;
+      this.last.next = newNode;
+      this.last = newNode;
     }
     this.length++;
     return this;
   }
   getTheIndex(index) {
     let counter = 0;
-    let lastNode = this.bottom;
+    let lastNode = this.last;
     while (counter !== index) {
       currentNode = currentNode.next;
       counter++;
@@ -33,10 +32,23 @@ class Queue {
     return currentNode;
   }
   dequeue() {
-    const holdingNode = this.top.next;
+    if (this.length === 0) {
+      return "This action is not possible because list length is 0";
+    }
+    this.first = this.first.next;
+    this.length--;
+    if (this.length === 0) {
+      this.last = null;
+    }
+    return this;
   }
   peek() {
-    return this.top;
+    return this.first;
   }
 }
 const myQueue = new Queue();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+myQueue.enqueue(4);
+myQueue.enqueue(5);
