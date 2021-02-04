7e2a69e9c900a4f1d72bfde48c1432bfc65dd32e
Otoniel Andrade <otonielandrade7@gmail.com>
2021-02-03 19:54:55 -0600

    singly linked list finished

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -15,7 +15,6 @@ class MySinglyLinkedList {
   }
   append(value) {
     const newNode = new Node(value);
-    debugger;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -23,11 +22,46 @@ class MySinglyLinkedList {
   }
   prepend(value) {
     const newNode = new Node(value);
-    debugger;
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
     return this;
   }
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const firstPointer = this.getTheIndex(index - 1);
+    const holdingPointer = firstPointer.next;
+    firstPointer.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+  remove(index) {
+    if (index >= this.length) {
+      return "This action is not possible because your index is higher to list length";
+    }
+    let nextToRemoveItem = this.getTheIndex(index + 1);
+    let beforeToRemoveItem = this.getTheIndex(index - 1);
+    beforeToRemoveItem.next = nextToRemoveItem;
+    this.length--;
+    return this;
+  }
 }
 let myLinkedList = new MySinglyLinkedList(1);
+myLinkedList.append(2);
+myLinkedList.append(3);
+myLinkedList.append(10);
+myLinkedList.append(4);
+myLinkedList.append(5);
